4399409f235e4e7ee23af2cf51e11aaad4b0393d
Adhyan <adhyan_srivastava95@yahoo.com>
2017-01-29 14:30:23 +0530

    Automated start/stop appEngine server and instrumented tests

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import com.example.adhyan.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
